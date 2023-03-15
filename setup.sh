ee0e415aa51d99b61f957112f74b78e607040445
Frank <chenjiayi_344@hotmail.com>
2023-03-15 13:03:42 +0100

    Add setup script for automating virtual environment and dependency setup
    Create virtual environment with Python 3.10

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a new virtual environment
+pip install virtualenv
+virtualenv --python=python3.10 env
+source env/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Set up the Django project
+#python manage.py migrate
+#python manage.py createsuperuser
