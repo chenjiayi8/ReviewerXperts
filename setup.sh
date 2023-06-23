486da8313a7c93ade698637b0452dfabb1ac4c26
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-06-23 10:13:45 +0200

    Update pip install command to use the requirements.txt file in the backend directory in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ virtualenv --python=python3.10 env
 source env/bin/activate
 
 # Install dependencies from requirements.txt
-pip install -r requirements.txt
+pip install -r ./backend/requirements.txt
 
 # Set up the Django project
 #python manage.py migrate
